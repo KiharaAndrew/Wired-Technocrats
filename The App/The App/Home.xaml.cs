9c1b39817e96a99109ba5de4d88703ec5e2dda76
Carson <casdre2020@gmail.com>
2019-04-06 16:25:29 +0300

    Added Home window and connected it.

---
+++ The App/The App/Home.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace The_App
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void details_page(object sender, RoutedEventArgs e)
+        {
+            stdntdtls.Content = new Student_Details();
+        }
+
+        private void win3(object sender, RoutedEventArgs e)
+        {
+            MainWindow win3 = new MainWindow();
+            win3.Show();
+        }
+    }
+}
